48b14afa4c42ab34484471c4d8fdddf9efbc3491
SALMERONCILLO <asalmen2611@g.educaand.es>
2025-02-13 15:15:46 +0100

    Add files via upload

---
+++ Actividad 3/script.js
@@ -0,0 +1,5 @@
+function pedirDatos() {
+    let nombre = prompt("Introduce tu nombre:");
+    let edad = prompt("Introduce tu edad:");
+    alert("Hola " + nombre + ", veo que tienes " + edad + " años.");
+}
